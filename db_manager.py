b321482eb9e97e4b56c9f1d9f46641099eceb1bc
BlockMaster <145277345+ProgrammProfi@users.noreply.github.com>
2025-07-06 22:31:25 +0300

    Add Telegram /start and script uploading. Update db_manager.

--- db_manager.py
+++ db_manager.py
@@ -3,7 +3,7 @@
 Class for interacting with the database
 """
 import sqlite3
-from config import DATABASE
+from config import DATABASE_PATH
 
 
 class DBManager:
@@ -15,19 +15,19 @@ class DBManager:
         conn = sqlite3.connect(self.database)
         with conn:
             conn.execute('''CREATE TABLE IF NOT EXISTS "scripts" (
-                            "script_id"          INTEGER NOT NULL UNIQUE,
+                            "script_id"   INTEGER NOT NULL UNIQUE,
                             "name"        TEXT NOT NULL,
+                            "user_id"     INTEGER NOT NULL,
                             "description" TEXT,
                             "script"      TEXT NOT NULL,
-                            "user_id"     INTEGER NOT NULL,
-                            PRIMARY KEY("script_id" AUTOINCREMENT)
-                            );''')
-            conn.execute('''CREATE TABLE "users" (
+                            "likes"       INTEGER NOT NULL DEFAULT 0,
+                            PRIMARY KEY("script_id" AUTOINCREMENT));''')
+            
+            conn.execute('''CREATE TABLE IF NOT EXISTS "users" (
                             "id"            INTEGER NOT NULL UNIQUE,
                             "username"      TEXT NOT NULL UNIQUE,
                             "scripts_count" INTEGER NOT NULL DEFAULT 0,
-                            PRIMARY KEY("id" AUTOINCREMENT)
-                            );''')
+                            PRIMARY KEY("id" AUTOINCREMENT));''')
             conn.commit()
     
     
@@ -47,30 +47,30 @@ class DBManager:
     
     
     def get_script_data(self, script_id):
-        return self.__select_data("SELECT script FROM scripts WHERE script_id = ?", (script_id,))
+        return self.__select_data("SELECT script FROM scripts WHERE script_id = ?", (script_id,))[0][0]
     
     
     def get_script_name(self, script_id):
-        return self.__select_data("SELECT name FROM scripts WHERE script_id = ?", (script_id,))
+        return self.__select_data("SELECT name FROM scripts WHERE script_id = ?", (script_id,))[0][0]
     
     
     def get_script_description(self, script_id):
-        return self.__select_data("SELECT description FROM scripts WHERE script_id = ?", (script_id,))
+        return self.__select_data("SELECT description FROM scripts WHERE script_id = ?", (script_id,))[0][0]
     
     
     def get_script_creator(self, script_id):
         return self.__select_data("""SELECT username FROM scripts
                                          INNER JOIN users ON users.id = scripts.user_id
-                                         WHERE script_id = ?""", (script_id,))
+                                         WHERE script_id = ?""", (script_id,))[0][0]
     
     
     def get_user_scripts(self, user_id):
-        return self.__select_data("""SELECT script_id FROM scripts
-                                         INNER JOIN users ON users.id = scripts.user_id
-                                         WHERE users.id = ?""", (user_id,))
+        return [item[0] for item in self.__select_data("""SELECT script_id FROM scripts
+                                                              INNER JOIN users ON users.id = scripts.user_id
+                                                              WHERE users.id = ?""", (user_id,))]
     
     
-    def get_scripts_rating(self):
+    def get_users_rating(self):
         return self.__select_data("""SELECT username, SUM(likes) AS total_likes
                                      FROM scripts
                                      INNER JOIN users ON users.id = scripts.user_id
@@ -79,17 +79,17 @@ class DBManager:
                                      LIMIT 10""")
     
     
-    def get_users_rating(self):
+    def get_scripts_rating(self):
         return self.__select_data("""SELECT name, likes FROM scripts
                                         ORDER BY likes DESC""")
     
     
     def get_user_id(self, username):
-        return self.__select_data("SELECT id FROM users WHERE username = ?", (username,))
+        return self.__select_data("SELECT id FROM users WHERE username = ?", (username,))[0][0]
     
     
     def get_script_id_by_name(self, script_name):
-        return self.__select_data("SELECT script_id FROM scripts WHERE name = ?", (script_name,))
+        return [item[0] for item in self.__select_data("SELECT script_id FROM scripts WHERE name = ?", (script_name,))]
     
     
     def add_script(self, name, data, user_id, description=""):
@@ -104,10 +104,23 @@ class DBManager:
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute("INSERT INTO users(username) VALUES (?)", (username,))
+            cur.execute("SELECT username FROM users WHERE username = ?", (username,))
+            res = cur.fetchone()
+            print(res)
+            if not res:
+                cur.execute("INSERT INTO users(username) VALUES (?)", (username,))
             conn.commit()
 
 
 if __name__ == '__main__':
-    manager = DBManager(DATABASE)
+    manager = DBManager(DATABASE_PATH)
     manager.create_tables()
+    print(manager.get_script_data(1))
+    print(manager.get_script_name(1))
+    print(manager.get_script_description(1))
+    print(manager.get_script_creator(1))
+    print(manager.get_user_scripts(1))
+    print(manager.get_users_rating())
+    print(manager.get_scripts_rating())
+    print(manager.get_user_id("Tema"))
+    print(manager.get_script_id_by_name("Test1"))
--- main.py
+++ main.py
@@ -2,3 +2,62 @@
 """"
 Main and the telegram bot logic
 """
+from db_manager import DBManager
+from config import *
+from telebot import TeleBot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
+bot = TeleBot(TOKEN)
+
+
+def gen_markup(rows):
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.row_width = 1
+    for row in rows:
+        markup.add(KeyboardButton(row))
+    return markup
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    manager.register_user(message.from_user.username)
+    bot.send_message(message.chat.id, """Привет! Я бот-менеджер полезных скриптов!
+Ты можешь сохранить здесь свой скрипт и поделиться им с остальным миром.)
+
+/start - вызвать это сообщение
+/upload_script - опубликовать скрипт
+
+Сделано @BlockMaster777
+
+Исходный код - github.com/BlockMaster777/Scrypto
+""")
+
+
+@bot.message_handler(commands=['upload_script'])
+def upload_script(message):
+    bot.send_message(message.chat.id, "Отправь название для скрипта")
+    bot.register_next_step_handler(message, upload_name_for_script)
+
+
+def upload_name_for_script(message):
+    name = message.text
+    user_id = manager.get_user_id(message.from_user.username)
+    bot.send_message(message.chat.id, "Отправь мне описание скрипта ('-' чтобы не указывать)")
+    bot.register_next_step_handler(message, upload_description_for_script, name=name, user_id=user_id)
+
+
+def upload_description_for_script(message, name, user_id):
+    description = message.text
+    bot.send_message(message.chat.id, "Отправь скрипт")
+    bot.register_next_step_handler(message, upload_script_final, name=name, user_id=user_id, description=description)
+
+
+def upload_script_final(message, name, user_id, description):
+    script = message.text
+    manager.add_script(name, script, user_id=user_id, description="" if description == "-" else description)
+    bot.send_message(message.chat.id, "Готово! Скрипт опубликован!")
+
+
+if __name__ == '__main__':
+    manager = DBManager(DATABASE_PATH)
+    bot.infinity_polling()
